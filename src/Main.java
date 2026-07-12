a47a22a0be02077cad3584901fb9ce002767cc6a
JDCar1241 <jcardenasv3.a@upao.edu.pe>
2026-07-11 19:15:11 -0500

    feat : subida de main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+
+import modelo.*;
+import modelo.PlanDatos;
+
+public class Main {
+
+    public static void mostrarPlanes(ProveedorFactory factory, int megas) {
+
+        PlanDatos personal = factory.crearPlanPersonal();
+        personal.setTasa();
+
+        PlanDatos negocio = factory.crearPlanNegocio();
+        negocio.setTasa();
+
+        PlanDatos academico = factory.crearPlanAcademico();
+        academico.setTasa();
+
+        System.out.println(personal + " Pago: " + personal.calculoPago(megas));
+        System.out.println(negocio + " Pago: " + negocio.calculoPago(megas));
+        System.out.println(academico + " Pago: " + academico.calculoPago(megas));
+    }
+
+    public static void main(String[] args) {
+
+        int megas = 1000;
+
+        System.out.println("===== LINEA RAPIDA =====");
+        mostrarPlanes(new LineaRapidaFactory(), megas);
+
+        System.out.println("\n===== CLARO =====");
+        mostrarPlanes(new ClaroFactory(), megas);
+
+        System.out.println("\n===== MOVISTAR =====");
+        mostrarPlanes(new MovistarFactory(), megas);
     }
 }
